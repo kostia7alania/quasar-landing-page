2c24417f2ad693860c8108a4f0a00a0a82668c36
Konstantin Bazrov <jimmy34694@gmail.com>
2021-10-23 22:38:45 +0300

    1st ver of the langing page is ready

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,11 @@
+// Mocks all files ending in `.vue` showing them as plain Vue instances
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue';
+  const component: ComponentOptions;
+  export default component;
+}
+
+declare module '*.jpg' {
+  const value: string;
+  export = value;
+}

30fed56e61369f9802e0b65910f6680d13a0adb4
Konstantin Bazrov <jimmy34694@gmail.com>
2021-11-09 01:03:32 +0300

    feat: added VButton

---
+++ components/ui/VButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button
+      v-bind="$attrs"
+       v-on="$listeners"
+    >
+      <slot/>
+
+    </button>
+</template>
+
+<script lang="ts">
+
+import {
+  defineComponent,
+  // PropType,
+  // computed,
+  // ref,
+  // toRef,
+  // Ref,
+} from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'VButton',
+
+})
+</script>
